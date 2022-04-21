9687d6025c6c9a28473ee2d2eb86d41ae55de235
YADAVBRIJESH1996 <59818822+YADAVBRIJESH1996@users.noreply.github.com>
2022-04-21 10:29:59 -0700

    first joke

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  const [joke,setjoke] = useState("Loding")
+  const [fname,setfn] = useState('yadav')
+  const [sname,setsn] = useState('john')
+  const newjoke = (first,second)=>{
+    fetch(`http://api.icndb.com/jokes/random?firstName=${first}&lastName=${second}`).then(res=> res.json())
+    // console.log(res)
+  .then(res1 =>{
+    // console.log(res1);
+   setjoke(res1.value.joke);
+  })
+}
+ useEffect(()=>{
+    newjoke(fname,setfn)
+ },[])
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h3 className='tage'>Yadav Progammar</h3>
+     <h1 className='h1'>Jokes App</h1>
+     <h2 className='link'>{joke}</h2>
+     <input type="text" className='in' value={fname} onChange = {(e)=>setfn(e.target.value)}></input>
+     <input type="text" className='in' value={sname} onChange = {(e)=>setsn(e.target.value)}></input>
+    <div ><button className='btn' onClick={()=>newjoke(fname,sname)}>Next joke😁</button></div> 
+     
     </div>
   );
 }
